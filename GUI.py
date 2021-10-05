41e124c35740bc70924ceafb243cd73801e73982
B.Jothin kumar <bjothinphysics@gmail.com>
2021-10-05 18:45:41 +0530

    Create GUI.py

---
+++ GUI.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+
+
+root = tk.Tk()
+root.wm_title('Desktop assistant')
+speak_command = None
+
+
+def set_speak_command(command):
+    global speak_command
+    speak_command = command
+
+
+speak_button = tk.Button(master=root, text='Speak', command=speak_command)
+speak_button.pack()
+output_label = tk.Label(master=root, text='')
+
+
+def display_output(output):
+    output_label['text'] = output
+
+
+output_label.pack()
+root.resizable(False, False)
+root.mainloop()
