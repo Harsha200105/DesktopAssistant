7abd317d40196f88714bc3a99c0ff6390f66b358
B.Jothin kumar <bjothinphysics@gmail.com>
2021-10-05 19:09:38 +0530

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -3,15 +3,13 @@ import tkinter as tk
 
 root = tk.Tk()
 root.wm_title('Desktop assistant')
-speak_command = None
+speak_button = tk.Button(master=root, text='Speak', command=lambda: pass)
 
 
 def set_speak_command(command):
-    global speak_command
-    speak_command = command
+    speak_button.configure(command=command)
 
 
-speak_button = tk.Button(master=root, text='Speak', command=speak_command)
 speak_button.pack()
 output_label = tk.Label(master=root, text='')
 
