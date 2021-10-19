e4f8310a337694abaa2a4c792ba3d30377a5924e
B.Jothin kumar <bjothinphysics@gmail.com>
2021-10-19 15:37:20 +0530

    Update Jarvis2.py

--- Jarvis2.py
+++ Jarvis2.py
@@ -79,7 +79,10 @@ def take_command():
         print("Listening....")
         r.pause_threshold = 0.5
         r.energy_threshold = 300
-        # The default value as per speech_recognition documentation. Increase if application stops responding.
+        """The default value as per speech_recognition documentation.
+        Increase if application stops responding. and decrease if 
+        assistant doesn't execute any command and just says next
+        command sir."""
 
         audio = r.listen(source)
 
